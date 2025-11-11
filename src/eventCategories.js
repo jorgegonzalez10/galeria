08f8a03bcdb586cba65a6830301f19a435778c6b
jorge gonzalez <gonzalezjor13@gmail.com>
2025-11-10 22:01:25 -0600

    inertar imagenes y galeria--active

--- src/eventCategories.js
+++ src/eventCategories.js
@@ -1,41 +1,24 @@
 import { continents } from './data/photos.js'
-import { categories } from './data/categories.js'
 
-const container = document.querySelector('.card')
-const photos = document.getElementById('.red')
 
+const container = document.querySelector('.container')
+const padre = document.querySelector('.galeria')
+const x = document.querySelector('.bottom')
 
 container.addEventListener('click', e => {
+const photos = document.querySelector('.red')
+padre.classList.add('galeria--active')
+photos.innerHTML = ''
+document.body.style.overflow = 'hidden'
+e.preventDefault()
+const target = e.target.dataset.category
+console.log(continents[target])
+continents[target].forEach((continent) => {
+photos.innerHTML += `<img class="red__img" src=${continent.image_path}>`
+})
+ })
 
-  e.preventDefault()
-  if (e.target.dataset.category === 'america'){
-    e.target.closest('.card').insertAdjacentHTML('afterend', `<div class="red" >
-        <img class="red__img" src=${continents['america'][0].image_path}>
-        <img class="red__img" src=${continents['america'][1].image_path}>
-        <img class="red__img" src=${continents['america'][2].image_path}>
-        <img class="red__img" src=${continents['america'][3].image_path}>
-        <img class="red__img" src=${continents['america'][4].image_path}>
-        <img class="red__img" src=${continents['america'][5].image_path}>
-        <img class="red__img" src=${continents['america'][6].image_path}>
-        <img class="red__img" src=${continents['america'][7].image_path}>
-        <img class="red__img" src=${continents['america'][8].image_path}>
-        <img class="red__img" src=${continents['america'][9].image_path}>
-      </div>`);
-  } else if(e.target.dataset.category === 'africa'){
-    e.preventDefault()
-    console.log(e)
-      e.target.closest('.card').insertAdjacentHTML('afterend', `<div class="red" >
-        <img class="red__img" src=${continents['africa'][0].image_path}>
-        <img class="red__img" src=${continents['africa'][1].image_path}>
-        <img class="red__img" src=${continents['africa'][2].image_path}>
-        <img class="red__img" src=${continents['africa'][3].image_path}>
-        <img class="red__img" src=${continents['africa'][4].image_path}>
-        <img class="red__img" src=${continents['africa'][5].image_path}>
-        <img class="red__img" src=${continents['africa'][6].image_path}>
-        <img class="red__img" src=${continents['africa'][7].image_path}>
-        <img class="red__img" src=${continents['africa'][8].image_path}>
-        <img class="red__img" src=${continents['africa'][9].image_path}>
-      </div>`)
-  }
-  console.log(e)
+x.addEventListener('click', e => {
+padre.classList.remove('galeria--active')
+document.body.style.overflow = 'auto'
 })
--- src/loadCategories.js
+++ src/loadCategories.js
@@ -5,13 +5,11 @@ const container = document.querySelector(".container")
 
 function cards(){
   categories.forEach(card => {
-    const html = document.createElement('a')
-    html.classList.add('card')
-    html.href = "#"
-    html.innerHTML = `
+    const html = document.createElement('div')
+    html.innerHTML = `<a href="#" class="card">
       <img data-category="${card.id}" src="${card.image_path}" class="card__img">
       <h2 class="card__continente" >${card.name}</h2>
-      <h4 class="card__photo-quantity">${card.quantity_photo}</h4>`
+      <h4 class="card__photo-quantity">${card.quantity_photo}</h4> </a>`
 
     container.appendChild(html)
   });
